56824baa90187bfa6aced9e09b83ebd8d9fcc13e
Hugo César Baraldi de Moraes <hugodemoraes@MacBook-Pro-de-Hugo.local>
2019-03-26 21:27:31 -0300

    launch API

---
+++ app/Models/Hooks/InviteHook.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const User = use('App/Models/User')
+const Kue = use('Kue')
+const Job = use('App/Jobs/InvitationEmail')
+
+const InviteHook = (exports = module.exports = {})
+
+InviteHook.sendInvitationEmail = async invite => {
+  const { email, team_id: teamId } = invite
+  const invited = await User.findBy('email', email)
+
+  if (invited) {
+    await invited.teams().attach(teamId)
+
+    return
+  }
+
+  const user = await invite.user().fetch()
+  const team = await invite.team().fetch()
+
+  Kue.dispatch(Job.key, { user, team, email }, { attempts: 3 })
+}
